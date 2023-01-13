fdee3c7ba46389860610c1d29f33b801af6cac68
Vũ Minh Đức <118959385+Vuduc2010@users.noreply.github.com>
2023-01-13 22:38:06 +0700

    Create HaSaKi.py

---
+++ HaSaKi.py
@@ -0,0 +1,17 @@
+#!/use/bin/python3
+#rewrite coded by vmd
+
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+import sys
+
+print('''\r\n
+
+        ╦ ╦ ╔═╗ ╔═╗ ╔═╗ ╦╔═ ╦
+        ╠═╣ ╠═╣ ╚═╗ ╠═╣ ╠╩╗ ║
+        ╩ ╩ ╩ ╩ ╚═╝ ╩ ╩ ╩ ╩ ╩
+    ╚══════════════════════════╝
